04a7f94fd03a0e4a2bdbf8beb6fba4c507055ec6
Oksana <git@github.com:OperaJAVA/Code-Like-a-Pro.git>
2024-09-28 17:22:35 +0300

    first commit

---
+++ app/src/main/java/ru/netology/titov/activity/MainActivity.kt
@@ -0,0 +1,82 @@
+package ru.netology.titov.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.result.launch
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.titov.R
+import ru.netology.titov.adapter.PostAdapter
+import ru.netology.titov.adapter.PostEventListener
+import ru.netology.titov.databinding.ActivityMainBinding
+import ru.netology.titov.dto.Post
+import ru.netology.titov.viewmodel.PostViewModel
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
+
+        val newPostContract = registerForActivityResult(NewPostActivityContract()) { text ->
+            text?.let {
+                viewModel.changeContent(it)
+                viewModel.saveContent()
+            }
+        }
+
+        val editPostActivityContract =
+            registerForActivityResult(EditPostActivityContract()) { text ->
+                text?.let {
+                    viewModel.changeContent(it)
+                    viewModel.saveContent()
+                }
+            }
+
+        val adapter = PostAdapter(
+            object : PostEventListener {
+
+                override fun onEdit(post: Post) {
+                    viewModel.editContent(post)
+//                    binding.group.visibility = View.VISIBLE
+                    editPostActivityContract.launch(post.content)
+                }
+
+                override fun onRemove(post: Post) {
+                    viewModel.removeById(post.id)
+                }
+
+                override fun onLike(post: Post) {
+                    viewModel.likeById(post.id)
+                }
+
+                override fun onShare(post: Post) {
+                    viewModel.shareById(post.id)
+                }
+
+                override fun onVideo(post: Post) {
+                    val intentVideo = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                    startActivity(intentVideo)
+                }
+            }
+        )
+
+        binding.container.adapter = adapter
+
+        viewModel.data.observe(this)
+        { posts ->
+            val newPost = adapter.itemCount < posts.size
+            adapter.submitList(posts) {
+                if (newPost) binding.container.smoothScrollToPosition(0)
+            }
+        }
+
+        binding.addPost.setOnClickListener {
+            newPostContract.launch()
+        }
+    }
+}
---
+++ app/src/main/java/ru/netology/titov/dto/Post.kt
@@ -0,0 +1,14 @@
+package ru.netology.titov.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val authorAvatar: String,
+    val published: String,
+    val content: String,
+    val liked: Boolean,
+    val video: String? = null,
+    val likeNum: Long,
+    val shareNum: Long,
+    val viewNum: Long
+)
